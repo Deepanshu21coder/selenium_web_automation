7e6ea376aadf775acd2c86d771b89322638fb085
Deepanshu21coder <132840096+Deepanshu21coder@users.noreply.github.com>
2025-03-10 11:21:31 +0530

    Create ConsultationTest.java

---
+++ ConsultationTest.java
@@ -0,0 +1,28 @@
+
+package com.hospital.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+```
